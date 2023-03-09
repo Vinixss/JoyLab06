995f06c1a121a464070f541feb46b89eb6575edd
Kaiden Joy <joykaiden@ufl.edu>
2023-03-09 16:58:00 -0500

    Added my encoder

---
+++ lab6_encoder.py
@@ -0,0 +1,53 @@
+
+stored_encoded_password = ''
+
+
+class LessThanEightError(Exception):
+    pass
+
+
+def encode_password(string_data):
+    encoded_password = ''
+
+    if len(string_data) < 8:
+        raise LessThanEightError
+
+    for character in string_data:
+        encoded_password += str((int(character) + 3) % 10)
+
+    return encoded_password
+
+
+def menu():
+    print('Menu\n'
+          '-------------\n'
+          '1. Encode\n'
+          '2. Decode\n'
+          '3. Quit\n')
+
+
+if __name__ == '__main__':
+    cont = True
+
+    while cont:
+        menu()
+        try:
+            user_input = int(input('Please enter an option: '))
+            if user_input < 1 or user_input > 3:
+                raise ValueError
+        except ValueError:
+            print('Error! Please enter a valid menu option!')
+            continue
+
+        try:
+            if user_input == 1:
+                password_input = input('Please enter your password to encode: ')
+                stored_encoded_password = encode_password(password_input)
+                print('Your password has been encoded and stored!\n')
+            elif user_input == 2:
+                print("NOT YET IMPLEMENTED")
+            elif user_input == 3:
+                cont = False
+
+        except LessThanEightError:
+            print('Error! Password must be 8 or more characters')
